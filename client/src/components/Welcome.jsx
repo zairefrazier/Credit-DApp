83818b0371f0742a9365f9fc27c84a3aba214978
Zaire <60757805+zairefrazier@users.noreply.github.com>
2022-11-16 13:46:18 -0500

    Commit

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -7,9 +7,6 @@ import { TransactionsContext } from "../context/TransactionsContext";
 import { Loader } from ".";
 
 
-
-
-
 const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
 const Input = ( { placeholder, name, type, value, handleChange } ) => (
