f28379559c792e9451518abe8b8e86b7e3076cf6
Zaire <60757805+zairefrazier@users.noreply.github.com>
2022-12-13 17:14:16 -0500

    Added Services

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -1,8 +1,41 @@
+import { BsShieldFillCheck} from 'react-icons/bs';
+import { BiSearchAlt} from 'react-icons/bi';
+import { RiHeart2Fill} from 'react-icons/ri';
+
+
+const ServiceCard = ({color, title, icon, Subtitle}) => (
+    <div className='flex flex-row justify-start items-center white-glassmorphism p-3 m-2 cursor-pointer hover:shadow-xl'>
+        <div className={'w-10 h-10 rounded-full flex justify-center items-center ${color}'}>
+            {icon}
+
+        </div>
+    </div>
+
+)
+
 const Services = () => {
     return (
-        <h1>
-            Services
-        </h1>
+        <div className="flex w-full justify-center items-center gradient-bg-services">
+            <div className="flex mf:flew-ro flew-col items-center justify-between md:pd-20 py-12">
+                <div className="flex-1 flex flex-col justify-start items-start">
+                    <h1 className="text-white text-3xl sm:text-5xl py-2">
+                        Imporoving 
+                        <br />
+                        As We Grow
+                    </h1>
+                </div>
+                
+            </div>
+            <div className='flex-1 flex flex-col justify-start items-center'>
+            <ServiceCard
+                color="bg-[#ff0000]"
+                title="Security Guaranteed"
+                icon ={<BsShieldFillCheck fontSize={21} className="text-white" />}
+                Subtitle = "Always maintain Privacy and Quality of Service"
+                />
+
+            </div>
+        </div>
     );
 }
 export default Services;
