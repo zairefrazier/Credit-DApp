dada439516ac650cce90f4c3ed36e8f33f546f73
Zaire <60757805+zairefrazier@users.noreply.github.com>
2022-12-22 03:14:50 -0500

    adjusted Services

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -2,14 +2,14 @@ import { BsShieldFillCheck} from 'react-icons/bs';
 import { BiSearchAlt} from 'react-icons/bi';
 import { RiHeart2Fill} from 'react-icons/ri';
 
-const ServiceCard = ({color, title, icon, Subtitle}) => (
+const ServiceCard = ({ color, title, icon, Subtitle}) => (
     <div className='flex flex-row justify-start items-center white-glassmorphism p-3 m-2 cursor-pointer hover:shadow-xl'>
         <div className={'w-10 h-10 rounded-full flex justify-center items-center ${color}'}>
             {icon}
         </div>
         <div className='ml-5 flex flex-col flex-1'>
             <h1 className='mt-2 text-white text-lg'>{title}</h1>
-            <p className='mt-2 text-white text-sm md:w 912'>{Subtitle}</p>
+            <p className='mt-2 text-white text-sm md:w-9/12'>{Subtitle}</p>
         </div>
     </div>
 )
@@ -17,19 +17,19 @@ const ServiceCard = ({color, title, icon, Subtitle}) => (
 const Services = () => {
     return (
         <div className="flex w-full justify-center items-center gradient-bg-services">
-            <div className="flex mf:flew-ro flew-col items-center justify-between md:pd-20 py-12">
+            <div className="flex mf:flew-row flew-col items-center justify-between md:pd-20 py-12 px-4">
                 <div className="flex-1 flex flex-col justify-start items-start">
                     <h1 className="text-white text-3xl sm:text-5xl py-2">
-                        Imporoving 
-                        <br />
-                        As We Grow
+                        Improving As 
+                        
+                        We Grow                        
                     </h1>
                 </div>
                 
             </div>
             <div className='flex-1 flex flex-col justify-start items-center'>
             <ServiceCard
-                color="bg-[#89845F8]"
+                color="bg-[#ff0000]"
                 title="Security Guaranteed"
                 icon ={<BsShieldFillCheck fontSize={21} className="text-white" />}
                 Subtitle = "Always maintain Privacy and Quality of Service"
@@ -37,13 +37,13 @@ const Services = () => {
                 <ServiceCard
                 color="bg-[#ff0000]"
                 title="Low Exchange Rates"
-                icon ={<BsShieldFillCheck fontSize={21} className="text-white" />}
+                icon ={<BiSearchAlt fontSize={21} className="text-white" />}
                 Subtitle = "Always maintain Privacy and Quality of Service"
                 />
                 <ServiceCard
                 color="bg-[#ff0000]"
                 title="Fast Transactions"
-                icon ={<BsShieldFillCheck fontSize={21} className="text-white" />}
+                icon ={<RiHeart2Fill fontSize={21} className="text-white" />}
                 Subtitle = "Always maintain Privacy and Quality of Service"
                 />
 
