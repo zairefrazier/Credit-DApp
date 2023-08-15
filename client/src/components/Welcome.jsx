41c662fd5a83574a90381d552f9cccb394a9fa6b
Zaire <60757805+zairefrazier@users.noreply.github.com>
2023-08-15 14:15:30 -0400

    typo fix

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -44,7 +44,7 @@ const Welcome = () => {
             <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
                 <div className="flex flex-1 justify-start flex-col mf:mr-10">
                     <h1 className="text-3xl sm:text-5xl text-black py-1">
-                        Send Cryto <br /> To Anyone, Anywhere
+                        Send Crypto <br /> To Anyone, Anywhere
                     </h1>
                     <p className="text-left mt-5 text-black font-light md:w-9/12 w-11/12 text-base">
                         Explore Crypto, With Credit Dap
